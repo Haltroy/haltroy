4d28af597d89cb992ed2f49c11b2bf8cc91e7601
haltroy <thehaltroy@gmail.com>
2022-06-01 18:37:48 +0300

    Sync 1.06.2022

--- tools/wyamfix/Program.cs
+++ tools/wyamfix/Program.cs
@@ -16,25 +16,27 @@ namespace wyamfix
             {
                 Console.WriteLine("WYAMFIX for HTAlt API Docs. ");
                 Console.WriteLine("----------------------------");
-                Console.WriteLine("USAGE: wyamfix [output path] [-v|--verbose]");
+                Console.WriteLine("USAGE: wyamfix [wyam output path] [output path] [-v|--verbose]");
                 Console.WriteLine("Example: wyamfix \"C:\\Users\\haltroy\\Desktop\\htalt\\\"");
             }
             else
             {
                 bool verbose = args.Contains("--verbose") || args.Contains("-v");
                 string workingDir = args[0];
+                string workingDir2 = args[1];
                 // Real work starts now
                 if (Directory.Exists(workingDir))
                 {
                     Stopwatch sw = new Stopwatch();
                     List<FileDirInfo> list = new List<FileDirInfo>();
-                    RecursiveFileSearch(workingDir, 0, ref list);
                     sw.Start();
+                    RecursiveFileSearch(workingDir, workingDir2, 0, ref list);
+
                     for (int i = 0; i < list.Count; i++)
                     {
                         var file = list[i];
-                        if (verbose) { Console.WriteLine((file.isDir ? "| " : "-\\ ") + file.FullPath); }
-                        if (!file.isDir && file.FullPath.EndsWith(".html"))
+                        if (verbose) { Console.WriteLine((file.IsDir ? "| " : "-\\ ") + file.FullPath); }
+                        if (!file.IsDir && file.FullPath.EndsWith(".html"))
                         {
                             string text = HTAlt.Tools.ReadFile(file.FullPath, System.Text.Encoding.UTF8);
                             for (int _i = 0; _i < list.Count; _i++)
@@ -43,7 +45,7 @@ namespace wyamfix
                                 text = text.Replace("href\"" + list[_i].Replace + "\"", "href\"" + list[_i].Path + "\"");
                                 text = text.Replace("src\"" + list[_i].Replace + "\"", "src\"" + list[_i].Path + "\"");
                             }
-                            HTAlt.Tools.WriteFile(file.FullPath, text, System.Text.Encoding.UTF8);
+                            HTAlt.Tools.WriteFile(file.FullPath.Replace(workingDir, workingDir2), text, System.Text.Encoding.UTF8);
                         }
                     }
 
@@ -55,14 +57,16 @@ namespace wyamfix
                     Console.WriteLine("Wyam Output directory doesn't exists. Argument(s): " + string.Join(' ', args));
                 }
             }
-            static void RecursiveFileSearch(string path, int depth, ref List<FileDirInfo> list, string workDir = "")
+            static void RecursiveFileSearch(string path, string opath, int depth, ref List<FileDirInfo> list, string workDir = "")
             {
                 if (list.FindAll(it => it.FullPath == (string.IsNullOrWhiteSpace(workDir) ? path : workDir)).Count > 0)
                 {
-                    var info = new FileDirInfo();
-                    info.FullPath = string.IsNullOrWhiteSpace(workDir) ? path : workDir;
-                    info.isDir = true;
-                    info.WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir;
+                    var info = new FileDirInfo
+                    {
+                        FullPath = string.IsNullOrWhiteSpace(workDir) ? path : workDir,
+                        IsDir = true,
+                        WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir
+                    };
                     info.FixPath();
                     info.Depth = depth;
                     list.Add(info);
@@ -70,22 +74,26 @@ namespace wyamfix
                 var folders = System.IO.Directory.GetDirectories(path, "*", SearchOption.TopDirectoryOnly);
                 for (int i = 0; i < folders.Length; i++)
                 {
-                    var info = new FileDirInfo();
-                    info.FullPath = folders[i];
-                    info.isDir = true;
-                    info.WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir;
+                    var info = new FileDirInfo
+                    {
+                        FullPath = folders[i],
+                        IsDir = true,
+                        WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir
+                    };
                     info.FixPath();
                     info.Depth = depth;
                     list.Add(info);
-                    RecursiveFileSearch(folders[i], depth + 1, ref list, string.IsNullOrWhiteSpace(workDir) ? path : workDir);
+                    RecursiveFileSearch(folders[i], opath, depth + 1, ref list, string.IsNullOrWhiteSpace(workDir) ? path : workDir);
                 }
                 var files = System.IO.Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly);
                 for (int i = 0; i < files.Length; i++)
                 {
-                    var info = new FileDirInfo();
-                    info.FullPath = files[i];
-                    info.isDir = false;
-                    info.WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir;
+                    var info = new FileDirInfo
+                    {
+                        FullPath = files[i],
+                        IsDir = false,
+                        WorkDir = string.IsNullOrWhiteSpace(workDir) ? path : workDir
+                    };
                     info.FixPath();
                     info.Depth = depth;
                     list.Add(info);
@@ -97,17 +105,17 @@ namespace wyamfix
         {
             public string FullPath { get; set; }
             public string Path { get; set; }
-            public bool isDir { get; set; }
+            public bool IsDir { get; set; }
             public string WorkDir { get; set; }
             public string Replace { get; set; }
             public int Depth { get; set; }
 
             public void FixPath(int depth = 0)
             {
-                WorkDir = (WorkDir.EndsWith("/") || WorkDir.EndsWith("\\")) ? WorkDir.Substring(0, WorkDir.Length - 1) : WorkDir;
+                WorkDir = (WorkDir.EndsWith("/") || WorkDir.EndsWith("\\")) ? WorkDir[..^1] : WorkDir;
                 Path = FullPath.Replace(WorkDir + "/", "").Replace(WorkDir + "\\", "");
-                Replace = !isDir
-                    ? (System.IO.Path.GetExtension(FullPath).ToLowerEnglish().EndsWith("html") ? ("/" + Path.Substring(0, Path.Length - System.IO.Path.GetExtension(FullPath).Length).Replace("\\", "/")) : ("/" + Path.Replace("\\", "/")))
+                Replace = !IsDir
+                    ? (System.IO.Path.GetExtension(FullPath).ToLowerEnglish().EndsWith("html") ? ("/" + Path[..^System.IO.Path.GetExtension(FullPath).Length].Replace("\\", "/")) : ("/" + Path.Replace("\\", "/")))
                     : "/" + Path.Replace("\\", "/");
 
                 string x = string.Empty;
@@ -122,7 +130,7 @@ namespace wyamfix
                         x += "../";
                     }
                 }
-                Path = x + Path.Replace("\\", "/") + (!isDir ? (!Path.EndsWith(System.IO.Path.GetExtension(FullPath)) ? System.IO.Path.GetExtension(FullPath) : "") : "/index.html");
+                Path = x + Path.Replace("\\", "/") + (!IsDir ? (!Path.EndsWith(System.IO.Path.GetExtension(FullPath)) ? System.IO.Path.GetExtension(FullPath) : "") : "/index.html");
             }
         }
     }
